f8188b294049021338e1656b4699ca5cd63c8940
Wendel Soares <wendel.soares.dev@gmail.com>
2022-02-28 11:34:29 -0300

    Começando a implementar o tabuleiro.

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,9 +1,9 @@
 ﻿using tabuleiro;
 
 
-Posicao P = new Posicao(3, 4);
+Tabuleiro tab = new Tabuleiro(8, 8);
 
-Console.WriteLine("Posição: " + P);
+    
 
-Console.ReadLine();
+    Console.ReadLine();
 
--- xadrez-console/tabuleiro/Posicao.cs
+++ xadrez-console/tabuleiro/Posicao.cs
@@ -3,15 +3,18 @@ namespace tabuleiro
 {
     class Posicao
     {
+        //auto properties
         public int linha { get; set; }
         public int coluna { get; set; }
 
+        //construtor
         public Posicao(int linha, int coluna)
         {
-            this.linha = linha; 
+            this.linha = linha;  //this = autorreferência
             this.coluna = coluna;   
         }
-
+        
+        //Sobreposição
         public override string ToString()
         {
             return linha
