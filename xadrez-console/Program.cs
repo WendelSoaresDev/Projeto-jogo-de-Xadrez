41fc7c6180c34bc4d0c3ea731105706a664af35f
Wendel Soares <wendel.soares.dev@gmail.com>
2022-02-28 13:20:51 -0300

    Colocando peças no tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,7 +1,13 @@
 ﻿using tabuleiro;
 using xadrez_console;
+using xadrez;
 
 Tabuleiro tab = new Tabuleiro(8, 8);
+
+tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+
 
 Tela.imprimirTabuleiro(tab);
 
